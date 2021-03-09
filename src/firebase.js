0cbf327ae0897769e925cd9334f7f33a7d7f3621
AYMEN <moniakamli95@gmail.com>
2021-03-09 09:23:02 +0100

    Implement the header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC8QlyNXlr2EcTkij5DzIzjufqYCvxUUlY",
+    authDomain: "clone-9cacb.firebaseapp.com",
+    projectId: "clone-9cacb",
+    storageBucket: "clone-9cacb.appspot.com",
+    messagingSenderId: "1016889039535",
+    appId: "1:1016889039535:web:1f2c21f245441df30e125a",
+    measurementId: "G-74X5MTGGSC"
+  };
