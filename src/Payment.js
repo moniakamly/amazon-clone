750483fa595443cc3c860a6190b937183f5539a2
AYMEN <moniakamli95@gmail.com>
2021-03-15 15:34:41 +0100

    fix errors of import

--- src/Payment.js
+++ src/Payment.js
@@ -1,14 +1,16 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React, { useEffect, useState } from 'react';
 import CurrencyFormat from 'react-currency-format';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import CheckoutProduct from './CheckoutProduct';
 import "./Payment.css";
 import { getBasketTotal } from './reducer';
 import { useStateValue } from './StateProvider';
+import axios from "./axios";
 
 function Payment() {
     const [{basket, user}, dispatch] = useStateValue();
+    const history = useHistory();
     
     const stripe = useStripe(); 
     const elements = useElements(); 
@@ -48,7 +50,7 @@ function Payment() {
             setSucceeded(true);
             setError(null);
             setProcessing(false);
-            
+
             history.replace('/orders')
         })
 
